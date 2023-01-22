f2171e6b41c3bb9a3e46fe87f85508ffe5044397
Gianni Gaspari <gianni.gaspari.s@gmail.com>
2023-01-22 15:06:17 -0300

    add JQuery

--- Módulo 09/assets/main.js
+++ Módulo 09/assets/main.js
@@ -1,4 +1,22 @@
 $(document).ready(function() {
-    console.log(document.querySelector('header button'))
-    console.log($('header button'))
+    $('header button').click(function(){
+        $('form').slideDown()  // comando para deslizar para baixo o formulário
+    })
+
+    $('#botao-fechar').click(function(){
+        $('form').slideUp()  // comando para deslizar para cima o formulário
+    })
+
+    $('form').on('submit', function(e){
+        console.log('submit')
+        e.preventDefault()
+    })
 })
+
+/*
+comando para chamar a ação em 2 clicks
+
+$('header button').dblclick(function(){
+    alert('Expandir formulário')
+})
+*/
