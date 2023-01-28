9c08ed03d92f18fce567f94622cb2b0ea2a1ff6f
Gianni Gaspari <gianni.gaspari.s@gmail.com>
2023-01-28 00:18:09 -0300

    JQuery Plugins

---
+++ Módulo 10/assets/js/main.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+    $('#carousel-images').slick({
+        autoplay: true,
+        autoplaySpeed: 4000,
+        fade: true,
+    })
+
+    $('.menu-hamburguer').click(function(){
+        $('nav').slideToggle()
+    })
+    
+    $('#telefone').mask('(00) 00000-0000')
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            mensagem: {
+                required: true
+            },
+            veiculoInteresse: {
+                required: false
+            }
+        },
+        messages: {
+            nome: 'Por favor, insira seu nome'
+        },
+        submitHandler: function(form) {
+            console.log(form)
+        },
+        invalidHandler: function(evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids()
+            if (camposIncorretos) {
+                alert(`Existem ${camposIncorretos} campos incorretos`)
+            }
+        }
+    })
+
+    $('.lista-veiculos button').click(function() {
+        const destino = $('#contato')
+        const nomeVeiculo = $(this).parent().find('h3').text()
+
+        $('#veiculo-interesse').val(nomeVeiculo)
+
+        $('html').animate({
+            scrollTop: destino.offset().top
+        }, 800)
+    })
+})
