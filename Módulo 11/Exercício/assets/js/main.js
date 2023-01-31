f355b9d96f30320a97838422c661dee80d485963
Gianni Gaspari <gianni.gaspari.s@gmail.com>
2023-01-31 01:07:10 -0300

    v2ExMod11

---
+++ Módulo 11/Exercício/assets/js/main.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+    $('#carousel-images').slick({
+        autoplay: true,
+        autoplaySpeed: 4000,
+        fade: true,
+    })
+    
+    $('.botao-menu button').click(function() {
+        $('.menu').slideToggle()
+    })
+
+    $('#telefone').mask('(00) 00000-0000')
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            }
+        },
+        messages: {
+            nome: 'Por favor, insira seu nome',
+            email: 'Por favor, insira um e-mail válido',
+            telefone: 'Por favor, insira um número de telefone válido'
+        }
+    })
+})
+
