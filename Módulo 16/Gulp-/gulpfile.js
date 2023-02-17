bc6a596d5dc2693ffc40c97dc6b5e71266c3617c
Gianni Gaspari <gianni.gaspari.s@gmail.com>
2023-02-17 19:33:30 -0300

    Update gulpfile.js

--- Módulo 16/Gulp-/gulpfile.js
+++ Módulo 16/Gulp-/gulpfile.js
@@ -1,6 +1,22 @@
 function funcaoPadrao(callBack) {
-    console.log('Executando via Gulp');
+    console.log('Executando via Gulp → VAI CARAIIIIII');
     callBack();
 }
 
+function dizOi(callBack) {
+    console.log('Olá Gulp, finalmente!');
+    dizTchau();
+    callBack();
+}
+
+function dizTchau() {
+    console.log('Tchau Gulp! Foi uma semana e tanto')
+}
+
 exports.default = funcaoPadrao;
+exports.dizOi = dizOi;
+
+/*
+Tarefas pulicas, são aquelas que exportamos.
+Tarefas privadas são aquelas que não exportamos, mas podemos utilizar dentro de outras tarefas
+*/
