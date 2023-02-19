e4d6ad63ad78a5202e8380ba5a4df1ea3947d643
Gianni Gaspari <gianni.gaspari.s@gmail.com>
2023-02-19 04:19:49 -0300

    Mod18-Grunt-initialCommit

--- Módulo 18/gruntfile.js
+++ Módulo 18/gruntfile.js
@@ -1,6 +1,34 @@
 module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        less: {
+            development: {
+                files: {
+                    'main.css': 'main.less'
+                }
+            },
+            production: {
+                options: {
+                    compress: true,
+                },
+                files: {
+                    'main.min.css': 'main.less'
+                }
+            }
+        },
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    'main2.css': 'main.scss'
+                }
+            } 
+        },
+        concurrent: {
+            target: ['olaGrunt', 'less', 'sass', 'tarefaDemorada']
+        }
     })
 
     grunt.registerTask('olaGrunt', function() {
@@ -11,5 +39,18 @@ module.exports = function(grunt) {
         }, 3000)
     })
 
-    grunt.registerTask('default', ['olaGrunt'])
+    grunt.registerTask('tarefaDemorada', function() {
+        const done = this.async();
+        setTimeout(function() {
+            console.log('olá grunt')
+            done();
+        }, 3000)
+    })
+
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-concurrent');
+
+    grunt.registerTask('default', ['concurrent']);
 }
