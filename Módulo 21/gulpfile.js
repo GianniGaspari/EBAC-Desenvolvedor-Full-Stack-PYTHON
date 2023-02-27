ca00ee1c967d91b46e21d8bcab2977beed29af12
Gianni Gaspari <gianni.gaspari.s@gmail.com>
2023-02-27 00:07:42 -0300

    Mod21-Aula2

--- Módulo 21/gulpfile.js
+++ Módulo 21/gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imageMin = require('gulp-imagemin')
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
@@ -7,7 +8,14 @@ function styles() {
         .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = styles
+function images() {
+    return gulp.src('./src/images/**/*')
+        .pipe(imageMin())
+        .pipe(gulp.dest('./dist/images'));
+}
+
+exports.default = gulp.parallel(styles, images);
+
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
 }
