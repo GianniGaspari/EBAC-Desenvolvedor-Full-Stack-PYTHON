1b96fc643b314f8db838d2217243757841f88ab1
Gianni Gaspari <gianni.gaspari.s@gmail.com>
2023-03-08 22:44:50 -0300

    COnfigurando o Uglify

--- Módulo 21/gulpfile.js
+++ Módulo 21/gulpfile.js
@@ -1,6 +1,13 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const imageMin = require('gulp-imagemin')
+const imageMin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+
+function scripts() {
+    return gulp.src('./src/scripts/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/js'))
+}
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
@@ -14,8 +21,9 @@ function images() {
         .pipe(gulp.dest('./dist/images'));
 }
 
-exports.default = gulp.parallel(styles, images);
+exports.default = gulp.parallel(styles, images, scripts);
 
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./src/styles/*.js', gulp.parallel(scripts))
 }
