9f55655ca4368abe2a6290719571f35a4d9a54c9
Gianni Gaspari <gianni.gaspari.s@gmail.com>
2023-03-23 21:32:39 -0300

    fetch API / tratando e lançando exceções

--- Módulo 23/main.js
+++ Módulo 23/main.js
@@ -20,18 +20,47 @@ $(document).ready(function() {
         $(botao).find('i').addClass('d-none');
         $(botao).find('span').removeClass('d-none');
 
-        $.ajax(endPoint).done(function(resposta) {
-            const logradouro = resposta.logradouro;
-            const bairro = resposta.bairro;
-            const cidade = resposta.localidade;
-            const estado = resposta.uf;
+        // $.ajax(endPoint).done(function(resposta) {
+        //     const logradouro = resposta.logradouro;
+        //     const bairro = resposta.bairro;
+        //     const cidade = resposta.localidade;
+        //     const estado = resposta.uf;
+        //     const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
+        //     $('#endereco').val(endereco);
+
+        //     setTimeout(function() {
+        //         $(botao).find('i').removeClass('d-none');
+        //         $(botao).find('span').addClass('d-none');
+        //     }, 1500);
+        // })
+        fetch(endPoint)
+        .then(function(resposta) {
+            return resposta.json();
+        })
+        .then(function(json) {
+            const logradouro = json.logradouro;
+            const bairro = json.bairro;
+            const cidade = json.localidade;
+            const estado = json.uf;
             const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
             $('#endereco').val(endereco);
-
+        })
+        .catch(function(erro) {
+            alert("Ocorreu um erro ao buscar o endereço, tente novamente mais tarde.")
+        })
+        .finally(function() {
             setTimeout(function() {
                 $(botao).find('i').removeClass('d-none');
                 $(botao).find('span').addClass('d-none');
-            }, 1500);
+            }, 1250);
         })
+    })
+
+    $('#formulario-pedido').submit(function(evento) {
+        evento.preventDefault();
+        
+        if ($('#nome').val().length == 0) {
+            throw new Error('Digite o nome');
+        }
     })
 })
