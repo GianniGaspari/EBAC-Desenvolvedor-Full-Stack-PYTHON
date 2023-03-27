0b8112a1cbf56885caed3c555473fe8b9571186f
Gianni Gaspari <gianni.gaspari.s@gmail.com>
2023-03-26 23:12:09 -0300

    Create arrays.js

---
+++ Módulo 25/src/arrays.js
@@ -0,0 +1,39 @@
+const  redesSociais = ['Facebook', 'Instagram', 'Twitter', 'Github'];
+
+for (let i = 0; i < redesSociais.length; i++) {
+    console.log(`Eu tenho perfil na rede social: ${redesSociais[i]}`)
+}
+
+// polimorfismo 
+// for (let i = 1; i <= redesSociais.length; i++) {
+//     console.log(`Eu tenho perfil na rede social: ${redesSociais[i - 1]}`)
+// }
+
+// redesSociais[0]
+// redesSociais[1]
+// redesSociais[2]
+// redesSociais[3]
+
+redesSociais.forEach(function(nomeDaRedeSocial, indice) {
+    console.log(`#${indice}: perfil na rede social: ${nomeDaRedeSocial}`)
+})
+
+const alunos = ['Gustavo', 'Olivia', 'Julia', 'Paula', 'Gianni', 'Wagner'];
+
+const alunos2 = alunos.map(function(itemAtual) { // map é bastante utilizado em manipulação de dados
+    return { // array de objetos
+        nome: itemAtual,
+        curso: 'Front-end' 
+    }
+})
+
+console.log(alunos2)
+
+
+const numeros = [1, 2, 3, 4, 5]
+const dobroNumeros = numeros.map(function(numeroAtual) {
+    // numeroAtual = numeroAtual * 2;
+    return numeroAtual * 2;
+})
+
+console.log(dobroNumeros)
